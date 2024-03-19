928a46b90e0b45ef8a907d0e0cb4fc41f1e30792
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-03-19 13:15:20 -0700

    added in ACS and FMR requests

--- src/services/ACSService.js
+++ src/services/ACSService.js
@@ -9,16 +9,17 @@ export async function fetchTwoBedroomRent(zipCode) {
   const year = '2021';
   const dataset = 'acs/acs5';
   const variables = 'B25031_004E,B25031_004M'; // B25031_004E for 2-bedroom rent, B25031_004M for its margin of error
-  const url = `${baseUrl}/${year}/${dataset}?get=${variables}&for=zip%20code%20tabulation%20area:${zipCode}&key=${apiKey}`;
+  // const url = `${baseUrl}/${year}/${dataset}?get=${variables}&for=zip%20code%20tabulation%20area:${zipCode}&key=${apiKey}`;
+  const url = `https://www.huduser.gov/portal/datasets/fmr/fmrs/FY2024_code/2024zip_code_calc.odn?zcta=46368&metro_code=METRO16980MM2960&year=2024`;
 
   try {
     const response = await axios.get(url);
-    const data = response.data[1]; // Assuming the first row of response data is headers and the second row is the actual data
-    console.log(data)
-    const estimatedRent = data[0];
-    const marginOfError = data[1];
+    console.log(response)
+    // const data = response.data[1]; // Assuming the first row of response data is headers and the second row is the actual data
+    // const estimatedRent = data[0];
+    // const marginOfError = data[1];
 
-    console.log(`Estimated 2-bedroom rent for ZIP code ${zipCode}: $${estimatedRent}, Margin of Error: $${marginOfError}`);
+    // console.log(`Estimated 2-bedroom rent in ${year} for ZIP code ${zipCode}: $${estimatedRent}, Margin of Error: $${marginOfError}`);
   } catch (error) {
     console.error('Error fetching data:', error);
   }
--- src/services/FMRService.js
+++ src/services/FMRService.js
@@ -9,17 +9,33 @@ export async function testFMR(){
     const year = '2021';
     const dataset = 'acs/acs5';
     const variables = 'B25031_004E,B25031_004M'; // B25031_004E for 2-bedroom rent, B25031_004M for its margin of error
-    const url = `${baseUrl}/${year}/${dataset}?get=${variables}&for=zip%20code%20tabulation%20area:${zipCode}&key=${apiKey}`;
+    // const url = `https://www.huduser.gov/hudapi/public/fmr/listCounties/IN`; // 1808999999
+    const url = `https://www.huduser.gov/hudapi/public/fmr/data/METRO16980MM2960`; 
+    // const url = `https://www.huduser.gov/hudapi/public/fmr/listMetroAreas`; 
   
     try {
-      const response = await axios.get(url);
-      const data = response.data[1]; // Assuming the first row of response data is headers and the second row is the actual data
+      const response = await axios.get(url, {
+        headers:{
+            Authorization: `Bearer ${apiKey}`
+        }
+      });
+      const data = response.data;
+    //   const garyArea = data.find(obj => obj.area_name === 'Gary, IN HUD Metro FMR Area');
+
+
       console.log(data)
-      const estimatedRent = data[0];
-      const marginOfError = data[1];
+      if(data?.data?.basicdata){
+        const fmrData = data?.data?.basicdata
+      const foundZip = fmrData.find(obj => obj.zip_code === '46368');
+
+        console.log(foundZip)
+      }
+      
   
-      console.log(`Estimated 2-bedroom rent for ZIP code ${zipCode}: $${estimatedRent}, Margin of Error: $${marginOfError}`);
+    //   console.log(`Estimated 2-bedroom rent for ZIP code ${zipCode}: $${estimatedRent}, Margin of Error: $${marginOfError}`);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
 }
+
+testFMR()
